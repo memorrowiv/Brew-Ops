9e9bf9dca1efbc2d36a5ee2f1b9fb52f0c3ed8d3
Matt Morrow <mmo2643@wgu.edu>
2025-02-11 23:53:01 -0500

    Added some initial firebase structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, Firestore } from 'firebase/firestore';
+import { environment } from '../environments/environment.prod';
 import { KegTrackerComponent } from './keg-tracker/keg-tracker.component';
 import { TapListComponent } from './tap-list/tap-list.component';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, KegTrackerComponent, TapListComponent], // Import your components here
+  imports: [
+    KegTrackerComponent,
+    TapListComponent,
+    AngularFirestoreModule,
+    RouterModule,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'brewery-front-of-house';
+  private firestore: Firestore;
+
+  constructor() {
+   
+    const app = initializeApp(environment.firebaseConfig);
+    this.firestore = getFirestore(app); // Initialize Firestore
+    console.log('Firebase initialized:', app);
+  }
+
+  ngOnInit() {
+  }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyAAwOCGbfL_PM14SjbHT7P_vZNJYpkalqc",
+        authDomain: "brewops-60b3f.firebaseapp.com",
+        projectId: "brewops-60b3f",
+        storageBucket: "brewops-60b3f.firebasestorage.app",
+        messagingSenderId: "228994002355",
+        appId: "1:228994002355:web:3969aa77d11a5adda4efc6",
+        measurementId: "G-70N0MFSHNS"
+    }
+};

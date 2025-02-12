5f93fda31cc79a45c0a8841eaa2dd04e40da57ee
Matt Morrow <mmo2643@wgu.edu>
2025-02-12 13:39:39 -0500

    Removed redunant function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,14 +32,4 @@ export class AppComponent implements OnInit {
   ngOnInit() {
   }
 
-  // Example of interacting with Firestore to save keg state
-  private async saveKegState(kegId: string, kegData: any): Promise<void> {
-    try {
-      const kegRef = doc(this.firestore, 'kegs', kegId);
-      await setDoc(kegRef, kegData); // Save keg data to Firestore
-      console.log('Keg state saved:', kegData);
-    } catch (error) {
-      console.error('Error saving keg state:', error);
-    }
-  }
 }
